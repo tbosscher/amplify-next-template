81ff615979b5cf372fade7e69f2859ef0e5f07e0
Trevor Bosscher <Trevor@Bosscher.local>
2025-01-30 10:33:57 -0500

    added storage new new new new

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -4,13 +4,5 @@ export const auth = defineAuth({
   loginWith: {
     email: true,
   },
-  signupAttributes: {
-    username: true,
-    email: true,
-    phone_number: true,
-  },
-  signInAliases: {
-    email: true,
-  },
-  // Ensure there are no syntax or type errors
+ 
 });
